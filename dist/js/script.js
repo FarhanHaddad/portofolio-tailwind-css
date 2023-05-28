6e735a6ecb637ac873d52f5fa4888246ac20e006
unknown <haddadyakan@gmail.com>
2023-05-28 11:41:10 +0700

    update darkmode

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,20 +1,57 @@
 //navbar fixed
-window.onscroll = function(){
-    const header = document.querySelector('header');
-    const fixednav = header.offsetTop;
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixednav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
 
-    if(window.pageYOffset > fixednav){
-            header.classList.add('navbar-fixed');
-    }else{
-        header.classList.remove('navbar-fixed');
-    }
+  if (window.pageYOffset > fixednav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
+  }
 };
 //hamburger
-const hamburger = document.querySelector('#hamburger');
-const navmenu = document.querySelector('#nav-menu');
+const hamburger = document.querySelector("#hamburger");
+const navmenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener('click', function(){
-    hamburger.classList.toggle('burger-aktif');
-    navmenu.classList.toggle('hidden');
-
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("burger-aktif");
+  navmenu.classList.toggle("hidden");
 });
+
+//klik diluar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navmenu) {
+    hamburger.classList.remove("burger-aktif");
+    navmenu.classList.add("hidden");
+  }
+});
+
+//darkmode toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+//pindahkan posisi toggle sesuai mode
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ["index.html"],
+  darkMode: "class",
   theme: {
-    container:{
+    container: {
       center: true,
-      padding: '16px',
-
+      padding: "16px",
     },
     extend: {
       colors: {
-        primary: '#14b8a6',
-        secondary: '#64748b',
-        dark: '#0f172a',
+        primary: "#14b8a6",
+        secondary: "#64748b",
+        dark: "#0f172a",
       },
-      screens:{
-        '2xl': '1320px',
+      screens: {
+        "2xl": "1320px",
       },
       fontFamily: {
-        wedding: ['Wedding','sans-serif'],
-        alex: ['Alex Brush','sans-serif'],
-        jack: ['Black Jack','sans-serif'],
-        dancing: ['Dancing Script OT','sans-serif'],
-        great: ['Great Vibes','sans-serif'],
-        inter: ['Inter Thin','sans-serif'],
-        nature: ['Nature Beauty Personal Use','sans-serif'],
-        opensan: ['Open Sans Light','sans-serif'],
-        ptmono: ['PT Mono','sans-serif'],
-        weday: ['Weddingday Personal Use Regular','sans-serif'],
-        dream: ['White Dream','sans-serif'],
-        
+        wedding: ["Wedding", "sans-serif"],
+        alex: ["Alex Brush", "sans-serif"],
+        jack: ["Black Jack", "sans-serif"],
+        dancing: ["Dancing Script OT", "sans-serif"],
+        great: ["Great Vibes", "sans-serif"],
+        inter: ["Inter Thin", "sans-serif"],
+        nature: ["Nature Beauty Personal Use", "sans-serif"],
+        opensan: ["Open Sans Light", "sans-serif"],
+        ptmono: ["PT Mono", "sans-serif"],
+        weday: ["Weddingday Personal Use Regular", "sans-serif"],
+        dream: ["White Dream", "sans-serif"],
       },
     },
   },
-  plugins: [],
-}
-
+  plugins: [require("@fortawesome/fontawesome-free")],
+};
