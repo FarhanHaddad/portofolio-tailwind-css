5cbe19415ddc47df6e1f79ad7dde23c119f7c864
FarhanHaddad <haddadyakan@gmail.com>
2024-06-28 22:06:39 +0700

    commit update portofolio & clients

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["index.html", "coba.html"],
   darkMode: "class",
   theme: {
     container: {
